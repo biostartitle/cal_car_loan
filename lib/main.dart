8af8b16e4deb7be46e92cdf411b31675c52967b8
biostartitle <s6419c10014@sau.ac.th>
2023-06-24 19:37:53 +0700

    1st commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+Run|Debug|Profile 
+void main() {
+  runApp(
+    
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: IntrodutionUI(),
+    )
+  )
+}
---
+++ lib/views/introduction_ui.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class HomeUI exteds StatefulWidget {
+  const HomeUI()
+}
